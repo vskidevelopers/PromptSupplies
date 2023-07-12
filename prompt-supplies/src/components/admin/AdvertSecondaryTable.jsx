d7e30802deb8395d88dcd5c8b5044749aacdbeb2
Ben <v.skidevelopers@gmail.com>
2023-07-12 11:47:43 +0300

    made additional changes to secondary table.

--- prompt-supplies/src/components/admin/AdvertSecondaryTable.jsx
+++ prompt-supplies/src/components/admin/AdvertSecondaryTable.jsx
@@ -155,7 +155,7 @@ function AdvertSecondaryTable({ advertItems, approved }) {
         />
       )}
 
-      <table className="max-w-4xl overflow-x-scroll">
+      <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
         <thead className="text-left ">
           <tr>
             <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
